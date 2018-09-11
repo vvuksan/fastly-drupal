7cdefa9cf6075f737ee94401df949776b13efb2d
V <vlemp@vuksan.com>
2018-09-11 09:25:39 -0400

    Revert "Issue #2978860 by kasperg: Warning when API response is not array"
    This reverts commit e4e470270d0196bbc5a5757a1792c6336ee2288e.
    
    `$result` should be an object here, not an array

--- src/Api.php
+++ src/Api.php
@@ -354,7 +354,7 @@ class Api {
         $response = $this->query('service/' . $this->serviceId . '/purge', [], 'POST', ["Surrogate-Key" => implode(" ", $keys)]);
         $result = $this->json($response);
 
-        if (is_array($result) && count($result) > 0) {
+        if (count($result) > 0) {
 
           $message = $this->t('Successfully purged following key(s) * @keys on %base_url. Purge Method: @purge_method', [
             '@keys' => implode(" ", $keys),
